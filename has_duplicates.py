7d298dce0f15248f607b0abab5850a4900591dea
chimaihueze <kihueze@gmail.com>
2025-03-22 22:31:12 +0000

    optimised solution for has_duplicates

--- has_duplicates.py
+++ has_duplicates.py
@@ -1,19 +1,13 @@
 """Given an integer array nums, return true if any value appears more than once in the array, otherwise return false."""
 
-nums = [1, 2, 3, 3]
-
-nums2 = [1, 2, 3, 4]
-
-
 def has_duplicate(list_of_numbers):
-    new_list = []
+    new_set = set(list_of_numbers)
 
-    for num in list_of_numbers:
-        if num in new_list:
-            return True
-        else:
-            new_list.append(num)
+    if len(new_set) != len(list_of_numbers): return True
     return False
 
+nums = [1, 2, 3, 3]
 print(has_duplicate(nums))
+
+nums2 = [1, 2, 3, 4]
 print(has_duplicate(nums2))
